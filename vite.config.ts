d3effed59c0412cf31ceb6a646aeb98fa6fa9d13
cyhuajuan <cyhuajuan@outlook.com>
2024-05-16 07:24:54 +0800

    chore: 更正一点细节

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
